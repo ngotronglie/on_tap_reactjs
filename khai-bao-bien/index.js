74973b00358f8169debff60f9845268dbd0fdbd2
Ngô Trọng Liêm <ngotrongliem2004@gmail.com>
2024-08-13 17:53:54 +0700

    khai bao bien

---
+++ khai-bao-bien/index.js
@@ -0,0 +1,39 @@
+var username = "Liem";
+
+{
+} // day la 1 khoi code
+{
+  {
+    {
+      {
+        var name = "Nguyen Van A";
+        let age = 18;
+      }
+    }
+  }
+}
+var name = "Nguyen Van B";
+
+console.log(name);
+console.log(username);
+// console.log(age); ---- loi khong goi dc
+
+console.log(password); // undefined
+
+var password = "123456";
+
+function hello() {
+  var name = "Nguyen Van C";
+  console.log(name);
+}
+hello();
+
+const hello__e = "ngo trong liem";
+// hello__e = "ngo trong liem 2" // khong the gan lai
+
+const myStudent = ["nguyen van a", "nguyen van b", "nguyen van c"];
+
+myStudent.push("nguyen van d");
+console.table(myStudent);
+
+
