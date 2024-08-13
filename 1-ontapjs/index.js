0910a2789d0b428d9b117b666f80791196518830
Ngô Trọng Liêm <ngotrongliem2004@gmail.com>
2024-08-13 17:35:17 +0700

    het phan ham

--- 1-ontapjs/index.js
+++ 1-ontapjs/index.js
@@ -11,6 +11,8 @@ console.log(sum(2, 3));
 /**
  * 1.regular function (named function)
  * 2.anonymous function(Expression function)
+ * 3. call back function
+ * 4.IIFE
  */
 
 const sum2 = function (a, b) {
@@ -28,3 +30,47 @@ console.log(sum3(2, 3));
 
 const sum4 = (a, b) => a + b;
 console.log(sum3(2, 3));
+
+const returnObject = () => {
+  name: "Nguyen Van A";
+  age: 18;
+};
+const returnObject2 = () => ({
+  name: "Nguyen Van A",
+  age: 18,
+});
+console.log(returnObject2());
+
+const returnArray = () => [1, 2, 3];
+console.log(returnArray());
+
+const demo = () => () => {
+  console.log("demo");
+};
+const demo2 = () => {
+  return () => {
+    return "demo2";
+  };
+};
+
+// call back function
+
+const congViecA = () => {
+  congViecB();
+  console.log("cong viec A");
+};
+
+const congViecB = () => {
+  setTimeout(() => {
+    console.log("cong viec B");
+  }, 2000);
+  // console.log("cong viec B");
+};
+
+congViecA();
+
+// IIFE // ham khong can goi
+
+(function () {
+  console.log("hello");
+})();
